97dc953c20971b49f0a8789423131b34ce1a7643
Robin Baumgartner <robin@baumgartners.ch>
2010-07-23 10:57:01 +0200

    Verschönerung, DRY, Bugfixes

--- com_heimbelegung/admin/views/belegung/tmpl/default.php
+++ com_heimbelegung/admin/views/belegung/tmpl/default.php
@@ -1,25 +1,8 @@
 <?php defined('_JEXEC') or die('Restricted access');
 
 JHTML::_('behavior.tooltip');
+require_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'helpers'.DS.'helpers.php');
 
-function date_mysql2german($date)
-{
-    $d    =    explode('-',$date);
-    return    sprintf('%02d.%02d.%04d', $d[2], $d[1], $d[0]);
-}
-
-function humanReadable($tageszeit)
-{
-    switch ($tageszeit)
-    {
-        case 'V':
-            return 'Vormittag';
-        case 'N':
-            return 'Nachmittag';
-        default:
-            return 'Vor- und Nachmittag';
-    }
-}
 ?>
 <script language="javascript" type="text/javascript">
 
